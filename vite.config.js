ea80e662a15b019b73aafdc5b747ff6ecd000b53
sharifulbb10 <sharifulbb10@gmail.com>
2025-03-30 19:13:48 +0600

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/h/',
-  plugins: [react(), tailwindcss(],
+  base: '/h/',
+  plugins: [react(), tailwindcss()],
 })
